29df1d8db354d0b0e8a1ebaeafe31eda345e952e
awtnb <arikaebanataw@gmail.com>
2025-11-06 15:21:18 +0900

    update

--- extract.py
+++ extract.py
@@ -2,7 +2,7 @@ import sys
 from pathlib import Path
 
 import pymupdf
-from pymupdf import Page, Rect
+from pymupdf import Annot, Page, Rect, Quad
 
 
 def text_by_rect(page: Page, rect: Rect) -> str:
@@ -21,24 +21,31 @@ def is_adjacent_rects(previous: Rect, current: Rect) -> bool:
     )
 
 
+# https://github.com/pymupdf/PyMuPDF/issues/318
+def to_minimal_rects(annots: list[Annot]) -> list[Rect]:
+    rects = []
+    for annot in annots:
+        vertices = annot.vertices
+        if not vertices:
+            continue
+        quad_count = int(len(vertices) / 4)
+        for i in range(quad_count):
+            q = vertices[i * 4 : i * 4 + 4]
+            rects.append(Quad(*q).rect)
+    return rects
+
+
 def extract_annots(path: str) -> None:
     pdf = pymupdf.Document(path)
 
     for i in range(pdf.page_count):
         page = pdf[i]
         highlight_annots = [a for a in page.annots() if a.type[1] == "Highlight"]
-        highlight_annots.sort(
-            key=lambda a: (
-                a.rect.top_left.y,
-                a.rect.top_left.x,
-                -1 * a.rect.height,
-                -1 * a.rect.width,
-            )
-        )
+        highlight_rects = to_minimal_rects(highlight_annots)
+        highlight_rects.sort(key=lambda a: (a.top_left.y, a.top_left.x))
 
         unified_rects = []
-        for annot in highlight_annots:
-            rect = annot.rect
+        for rect in highlight_rects:
             if len(unified_rects) < 1:
                 unified_rects.append(rect)
                 continue
